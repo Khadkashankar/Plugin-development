6dfcfc2a501b8e133306dc1520d9abe4a229d813
Khadkashankar <shankar.755421@snsc.tu.edu.np>
2024-01-09 11:15:11 +0545

    added user register

---
+++ task3_plugins/wordpress/wp-content/plugins/user-resgister/user-form.php
@@ -0,0 +1,56 @@
+<?php
+
+if(isset($_POST['submit'])){
+   global $wpdb;
+    
+    $email = $wpdb->escape($_POST['email']);
+    $password = $wpdb->escape($_POST['password']);
+    $user_name = $wpdb->escape($_POST['user_name']);
+    $display_name = $wpdb->escape($_POST['display_name']);
+    $first_name = $wpdb->escape($_POST['first_name']);
+    $last_name = $wpdb->escape($_POST['last_name']);
+    $role = $wpdb->escape($_POST['role']);
+
+
+
+    $user_data = array(
+        'user_login'=>$user_name,
+        'user_pass'=>$password,
+        'user_nicename'=>$first_name.' '.$last_name,
+        'user_email'=>$email,
+        'display_name'=>$display_name,
+        
+    );
+    $res=wp_insert_user($user_data);
+    // print_r($res);
+    if($res){
+        echo "user registered successfully";
+    }
+    
+}
+
+
+?>
+
+<form action="<?php echo get_the_permalink(); ?>" method="POST">
+    <br><br>
+    <label for="">Email:</label>
+    <input type="email" name="email" required> <br><br>
+    <label for="">Password:</label>
+    <input type="password" name="password" required> <br><br>
+    <label for="">UserName:</label>
+    <input type="text" name="user_name" required> <br><br>
+    <label for="">DisplayName:</label>
+    <input type="text" name="display_name" required> <br><br>
+    <label for="">FirstName:</label>
+    <input type="text" name="first_name" required> <br><br>
+    <label for="">LastName:</label>
+    <input type="text" name="last_name" required> <br><br>
+    <label for="">select role</label>
+    <select name="role" id="">
+        <option value="subscriber">Subscriber</option>
+        <option value="editor">Editor</option>
+        <option value="dministrator">Administrator</option>
+    </select><br><br>
+    <input type="submit" name="submit" value="Send">
+</form>
